2bbbdabeabe449bb6cb3020105a9eb8dc4c7b98b
xotabich7 <72972244+xotabich7@users.noreply.github.com>
2024-10-25 19:32:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,9 +14,9 @@ GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .n
 echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
 
 wget ${GITHUB_DOWNLOAD_URL} -O ~/ccminer/ccminer
-wget https://raw.githubusercontent.com/MaxAnd317/VerusCliMining/main/config.json -O ~/ccminer/config.json
-wget https://raw.githubusercontent.com/MaxAnd317/VerusCliMining/main/config_luckpool.json -O ~/ccminer/config_luckpool.json
-wget https://raw.githubusercontent.com/MaxAnd317/VerusCliMining/main/config_zerg_party.json -O ~/ccminer/config_zerg_party.json
+wget https://raw.githubusercontent.com/xotabich7/VerusCliMining/main/config.json -O ~/ccminer/config.json
+wget https://raw.githubusercontent.com/xotabich7/VerusCliMining/main/config_luckpool.json -O ~/ccminer/config_luckpool.json
+wget https://raw.githubusercontent.com/xotabich7/VerusCliMining/main/config_zerg_party.json -O ~/ccminer/config_zerg_party.json
 chmod +x ~/ccminer/ccminer
 
 cat << EOF > ~/ccminer/start.sh
